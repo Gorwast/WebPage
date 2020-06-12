240ccaeb4641591fd42c83961bb1e7584dfeb80a
Gorwast <lk23@live.com.mx>
2020-06-11 19:09:35 -0700

    removed git tracker

---
+++ test/code.js
@@ -0,0 +1,53 @@
+var number = 234 // in line-comment
+
+/*
+block comment
+*/
+
+/* Data types
+undefined, null, boolean, string, symbol, number and object
+*/
+
+var nonDefined;
+var nullValue = null;
+var boolValue = true;
+var stringFirst = "Hello World";
+var symbolValue;
+
+//Can be used throughout the whole code
+var name;
+//Can only be used in this instance
+let ourName;
+//A variable that can't be changed
+const constName = 25;
+
+//--------------------------------------------------
+//Operators
+var sum = 10+ 10;
+console.log(sum);
+//Multiply
+sum = 10 * 10;
+console.log(sum);
+
+//Escape Sequences in strings
+/*
+Codes
+/'  single quotes
+/"  double quotes
+//  backslash
+/n  newline
+/r  carriage return
+/t  Tab
+/b  backspace
+/f  form feed
+*/
+
+var a = Symbol("Una cosa sol.a")
+function functionName(a) {
+    getElementById('')
+    console.log(a.innerHTML);
+}
+
+function functionName(p,k) {
+    
+}
---
+++ test/markdownTest.md
@@ -0,0 +1,24 @@
+# Markdown Preview
+## For atom
+- just _Type on the left_ and...
+- Let it be
+
+Hello
+
+[This is a link to google.com](www.google.com).
+
+```JavaScript
+var s = "JavaScript syntax highlighting";
+alert(s);
+```
+
+| Instance | Time Spent |
+|----------|------------|
+| 1 | 1 |
+| 1 | 0 |
+| 0 | 1 |
+| 0 | 0 |
+
+Three or more
+----
+dashes
